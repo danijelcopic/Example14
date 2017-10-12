815129dd03d3e0caefb74c2e049530dab41d8b0d
Danijel Copic <danijelcopic@gmail.com>
2017-10-13 01:45:07 +0200

    Complet only second activity layout

--- app/src/main/java/rs/aleph/android/example12/activities/SecondActivity.java
+++ app/src/main/java/rs/aleph/android/example12/activities/SecondActivity.java
@@ -1,21 +1,12 @@
 package rs.aleph.android.example12.activities;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Toast;
-
 
 import rs.aleph.android.example12.R;
 
-import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
-
 
 public class SecondActivity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +14,5 @@ public class SecondActivity extends Activity {
         setContentView(R.layout.activity_second);
 
     }
-
-
 }
 
